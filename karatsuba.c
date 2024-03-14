e629bdd535403149244880eee1221429b73b1a34
HenriqueFrancaa <henrique23.franca@gmail.com>
2024-03-14 10:18:20 -0300

    Karatsuba

--- karatsuba.c
+++ karatsuba.c
@@ -2,114 +2,159 @@
 #include <string.h>
 #include <stdlib.h>
 #include <math.h>
+#define MAX_SIZE 65536 
 
-typedef struct 
-{
-    char num[1024];
-    char c[1];
-    int sz;
-    int half;
-}numero;
 
-numero xl,xr,yl,yr,tempx,tempy;
+typedef struct{
+    char num[MAX_SIZE];
+    int size;
+} str;
 
-void separa(char *n, int sz, int start, int end){
-    numero str;
-    memset(str.num,'\0',sz+1);
-    for(int i = 0; i < (end-start); i++){
-        str.num[i] = n[i+start];
+void PrintS(str n){
+    int i = 0;
+    while(n.num[i] == '0'){ i++; }
+    if(i == n.size){ 
+        printf("0\n"); return;
     }
-    //printf("half = %s\n", str.num);
-    strcpy(n,str.num);
-    //printf("%s", n);
+    for(i; i < n.size; i++){
+        printf("%c", n.num[i]);
+    } printf("\n");
 }
 
-void karatsuba(char *x, char *y, int sz){
-    if(sz == 1){
-        //algo
+void igualando(char* str1, char* str2, int size){
+    if(size == 1){ 
+        return;
+     }
+    int p = 1;
+    while(size > pow(2, p)){ 
+        p++; 
     }
-    int m = sz/2;
-    strcpy(tempx.num,x);
-    separa(x,sz,0,m);
-    strcpy(xl.num,x);
-    
-    separa(tempx.num,sz,m,sz);
-    strcpy(xr.num,tempx.num);
-    printf("%s e %s\n", xl.num, xr.num);
-
-    strcpy(tempy.num,y);
-    separa(y,sz,0,m);
-    strcpy(yl.num,y);
-    
-    separa(tempy.num,sz,m,sz);
-    strcpy(yr.num,tempy.num);
-
-    printf("%s e %s\n", yl.num,yr.num);
+    int zeros = pow(2, p);
+    str power1;
+    str power2;
+    memset(power1.num, '\0', sizeof(power1.num));
+    memset(power2.num, '\0', sizeof(power2.num));
+    memset(power1.num, '0', zeros - strlen(str1));
+    memset(power2.num, '0', zeros - strlen(str2));
+    strcat(power1.num, str1);
+    strcat(power2.num, str2);
+    strcpy(str1, power1.num);
+    strcpy(str2, power2.num);
 }
 
-void soma(char *x, char *y, int sz, char *s){
-    int carry = 0, e = 0;
-    numero carga;
-    memset(s,'\0',sz+1);
-    for(int i = sz-1; i >= 0; i--){
-        e = (x[i] - '0') + carry + (y[i] - '0');
-        if(e >= 10){
-            carry = e/10;
-            s[i] = (e%10) + '0';
+str Zeros(str str, int size, int n){
+    for(int i = 0; i < size / n ; i++){
+        str.num[i+str.size] = '0';
+    }
+    str.size += size / n;
+    return str;
+}
+
+str halph(str n, int pos){
+    str s; 
+    memset(s.num, '\0', sizeof(s.num));
+    s.size = n.size / 2;
+
+    if(pos == 0){
+        for(int i = 0; i < s.size; i++){
+            s.num[i] = n.num[i];
         }
-        else{
-            carry = 0;
-            s[i] = e + '0';
+    }else{
+        for(int i = 0; i < s.size; i++){
+            s.num[i] = n.num[i + s.size];
         }
+    }
+    return s;
+}
+
+str soma(str a, str b){
+    if(a.size > b.size){
+        igualando(a.num, b.num, a.size);
+        a.size = strlen(a.num);
+        b.size = strlen(b.num); 
+    }
+    else if(a.size < b.size){
+        igualando(a.num, b.num, b.size);
+        a.size = strlen(a.num);
+        b.size = strlen(b.num); 
+    }
+    int carry = 0;
+    str r; 
+    memset(r.num, '\0', sizeof(r));
+    str result;
+    memset(result.num, '\0', sizeof(result));
+    for(int i = a.size-1; i >= 0; i--){
+        int sum = (a.num[i] - '0') + (b.num[i] - '0');
+        r.num[i] = ((sum + carry) % 10) + '0';
+        carry = (sum + carry) / 10;
     }
     if(carry != 0){
-        carga.c[0] = '1';
-        strcat(carga.c,s);
-        strcpy(s,carga.c);
+        result.num[0] = '1';
+        strcat(result.num, r.num);
+        result.size = strlen(result.num);
+    }else{
+        strcpy(result.num, r.num);
+        result.size = strlen(result.num);
     }
-
-
+    return result;
 }
 
-void igualando(char *n, int sz, int pot){
-    numero zeros;
-    memset(zeros.num,'\0', 1024);
-    memset(zeros.num,'0', pot-sz);
-    strcat(zeros.num,n);
-    strcpy(n,zeros.num);
+str mult(str x, str y){
+        str product; memset(product.num, '\0', sizeof(product.num));
+        int r = (x.num[0] - '0') * (y.num[0] - '0');
+        if(r >= 10){
+            product.num[0] = r / 10 + '0';
+            product.num[1] = r % 10 + '0';
+        }else{
+            product.num[0] = '0';
+            product.num[1] = r + '0';
+        }
+        product.size = 2;
+        return product;
 }
 
-int potencia(int size){
-    int x;
-    for(int i = 1; i < 10; i++){
-        x = pow(2,i);
-        if(x >= size)return x;
+str karatsuba(str x, str y){
+    if(x.size == 1){
+        str result = mult(x, y);
+        return result;
     }
+    str a, b, c, d;
+    a = halph(x, 0); 
+    b = halph(x, 1);
+    c = halph(y, 0); 
+    d = halph(y, 1);
+    str ac = karatsuba(a, c); 
+    str ad = karatsuba(a, d); 
+    str bc = karatsuba(b, c); 
+    str bd = karatsuba(b, d); 
+    
+    str adbc = Zeros(soma(ad, bc), x.size, 2); //10 elevado a N/2 
+    str r1 = Zeros(ac, x.size, 1); // 10 elevado a N
+    str r2 = soma(r1, adbc); 
+    str r3 = soma(r2, bd);
+    return r3;
 }
 
-numero n1,n2,max,pot,sum;
 int main(){
-    scanf("%s", &n1.num);
-    scanf("%s", &n2.num);
-    n1.sz = strlen(n1.num);
-    n2.sz = strlen(n2.num);
+    str x; scanf("%s", x.num); 
+    str y; scanf("%s", y.num); 
+    x.size = strlen(x.num);
+    y.size = strlen(y.num);
+  
+    printf("X = %s\n", x.num);
+    printf("Y = %s\n", y.num);
 
-    if(n1.sz > n2.sz)max.sz = n1.sz;
-    else max.sz = n2.sz;
-    //printf("NUMEROS : %s + %s\n", n1.num, n2.num);
-    //printf("TAMANHO :%d %d\n", n1.sz, n2.sz);
+    if(x.size > y.size){
+        igualando(x.num, y.num, x.size);
+    }else{
+        igualando(x.num, y.num, y.size);
+    }
     
-    pot.sz = potencia(max.sz);
-    igualando(n1.num,n1.sz,pot.sz);
-    igualando(n2.num,n2.sz,pot.sz);
+    x.size = strlen(x.num);
+    y.size = strlen(y.num);
 
-    karatsuba(n1.num,n2.num,pot.sz);
-    
-    /*
-    soma(n1.num,n2.num,pot.sz,sum.num);
+    str result = karatsuba(x, y);
+    printf("\nX * Y = "); PrintS(result); printf("\n");
 
-    //printf("NOVO : %s %s\n", n1.num,n2.num);
-    printf("SOMA: %s\n", sum.num);
-    */
     return 0;
 }
